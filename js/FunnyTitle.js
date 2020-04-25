3848503de9945fedaf81fd37476eae1a778e134d
mac <mac@macdeMacBook-Air.local>
2020-04-25 22:23:35 +0800

    Site updated: 2020-04-25 22:23:35

---
+++ js/FunnyTitle.js
@@ -0,0 +1,17 @@
+<!--浏览器搞笑标题-->
+ var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         document.title = 'ヽ(●-`Д´-)ノ页面崩溃啦！';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         document.title = 'ヾ(´∀`o)ノ开个玩笑！' + OriginTitle;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+ });
